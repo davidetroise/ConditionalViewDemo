482e13e8130ff083dc6bf80fc555c12731606141
Davide Troise <davide@troise.net>
2022-12-27 02:31:43 +0100

    Initial Commit

---
+++ ConditionalViewDemo/ConditionalViewDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ConditionalViewDemoApp.swift
+//  ConditionalViewDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct ConditionalViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
